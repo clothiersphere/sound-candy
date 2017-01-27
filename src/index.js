23c82e03f89bcdfe568e15138982bb4b46bedb19
ChristopherLee <xchrislee@gmail.com>
2017-01-26 16:16:46 -0800

    first commit;

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
